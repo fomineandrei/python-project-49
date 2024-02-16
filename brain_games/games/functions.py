20e5949f8631c4afc2768faa2921b2193dc7ce4d
fomineandrei <fomineandrei@yandex.ru>
2024-02-16 12:16:17 +0300

    fix functions.py

--- brain_games/games/functions.py
+++ brain_games/games/functions.py
@@ -15,11 +15,11 @@ def welcome_user():
 def game_check(question_check_list):
     for question, check in question_check_list:
         print(f'Question: {question}')
-        answer = prompt.string('Your answer: ')
-        if answer == check:
+        answer = input('Your answer: ')
+        if str(answer) == check:
             print('Correct!')
             continue
-        elif answer != check:
+        elif str(answer) != check:
             print(f"'{answer}' is wrong answer;(. Correct answer is '{check}'.")
             print(f"Let's try again, {name}!")
             break
