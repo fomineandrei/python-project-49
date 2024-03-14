cd3a2a558259b96fd55fed0e15a89bcda9aa6f50
fomineandrei <fomineandrei@yandex.ru>
2024-03-14 13:32:33 +0300

    refactored brain_progression_logic.py

--- brain_games/games/brain_progression_logic.py
+++ brain_games/games/brain_progression_logic.py
@@ -8,36 +8,36 @@ RULES = 'What number is missing in the progression?'
 # Длина прогрессии
 PROG_LENGTH = 10
 # Начало прогрессии
-START_RANGE = 70
+START_RANGE = [1, 70]
 # Шаг прогрессии
-STEP_RANGE = 15
+STEP_RANGE = [1, 15]
 # Индекс скрываемого числа прогрессии
-INDEX_RANGE = PROG_LENGTH - 1
+INDEX_RANGE = [1, PROG_LENGTH - 1]
 
 
 # Функция создания арифметической прогрессии
 def random_progression(start, step, length):
-    start_prog = random.randint(1, start)
-    step_prog = random.randint(1, step)
     progression = []
     i = 0
     while i < length:
-        progression.append(str(start_prog))
-        start_prog = start_prog + step_prog
+        progression.append(str(start))
+        start = start + step
         i += 1
     return progression
 
 
 # Функция создания строки вопроса
 def progression_string(progression, index):
-    random_index = random.randint(1, index)
-    check = progression[random_index]
-    progression[random_index] = '..'
+    check = progression[index]
+    progression[index] = '..'
     return " ".join(progression), check
 
 
 # Функция заполнения листа вопросов
 def question_check():
-    progression = random_progression(START_RANGE, STEP_RANGE, PROG_LENGTH)
-    question, check = progression_string(progression, INDEX_RANGE)
+    start_rand = random.randint(*START_RANGE)
+    step_rand = random.randint(*STEP_RANGE)
+    index_rand = random.randint(*INDEX_RANGE)
+    progression = random_progression(start_rand, step_rand, PROG_LENGTH)
+    question, check = progression_string(progression, index_rand)
     return (question, check)
