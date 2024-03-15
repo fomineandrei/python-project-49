5154770a85ceb3b7a495839578547236aafc20ab
fomineandrei <fomineandrei@yandex.ru>
2024-03-15 13:01:09 +0300

    refactored brain_gcd_logic.py

--- brain_games/games/brain_gcd_logic.py
+++ brain_games/games/brain_gcd_logic.py
@@ -6,11 +6,15 @@ import random
 
 # Условие игры Наибольший Общий Делителm
 RULES = 'Find the greatest common divisor of given numbers.'
+# Диапазоны для множителя и чисел
+RANGE_MULT = (1, 10)
+RANGE_NUM1 = (2, 10)
+RANGE_NUM2 = (2, 10)
 
 
 # Функция создания списка пар чисел для нахождения НОД
 def question_answer_pair():
-    multiplier = random.randint(1, 10)
-    a = multiplier * random.randint(2, 10)
-    b = multiplier * random.randint(2, 10)
+    multiplier = random.randint(*RANGE_MULT)
+    a = multiplier * random.randint(*RANGE_NUM1)
+    b = multiplier * random.randint(*RANGE_NUM2)
     return (f'{a} {b}', str(math.gcd(a, b)))
