6c73c482694e054473705199217bc377a63332b9
fomineandrei <fomineandrei@yandex.ru>
2024-03-15 12:16:03 +0300

    refactored brain_calc_logic.py

--- brain_games/games/brain_calc_logic.py
+++ brain_games/games/brain_calc_logic.py
@@ -6,25 +6,34 @@ import itertools
 
 # Условие игры Калькулятор
 RULES = 'What is the result of the expression?'
-# Циклический итератор вариантов математических операций
-OPERATION_VARIANTS = itertools.cycle(['summ', 'diff', 'mult'])
+# Интервалы чисел для различных опреаций
+RANGE_SUMM = [(20, 200), (20, 200)]
+RANGE_DIFF = [(300, 500), (50, 299)]
+RANGE_MULT = [(10, 99), (2, 10)]
+# Циклический итератор вариантов математических операций и числовых интервалов
+OPERATION_VARIANTS = itertools.cycle(
+    [('summ', RANGE_SUMM), ('diff', RANGE_DIFF), ('mult', RANGE_MULT)])
+
+
+# Вычисление правильного ответа и запись вопроса
+def ariph_calculate(arg1, arg2, ariph_operation):
+    if ariph_operation == 'summ':
+        summ = f'{arg1} + {arg2}'
+        return (summ, str(arg1 + arg2))
+    elif ariph_operation == 'diff':
+        diff = f'{arg1} - {arg2}'
+        return (diff, str(arg1 - arg2))
+    elif ariph_operation == 'mult':
+        mult = f'{arg1} * {arg2}'
+        return (mult, str(arg1 * arg2))
 
 
 # Функция создания кортежа "вопрос - правильный ответ"
 def question_answer_pair():
-    a = random.randint(20, 200)
-    b = random.randint(20, 200)
-    c = random.randint(300, 500)
-    d = random.randint(50, 299)
-    u = random.randint(10, 99)
-    v = random.randint(2, 10)
-    operation = next(OPERATION_VARIANTS)
-    if operation == 'summ':
-        summ = f'{a} + {b}'
-        return (summ, str(a + b))
-    elif operation == 'diff':
-        diff = f'{c} - {d}'
-        return (diff, str(c - d))
-    elif operation == 'mult':
-        mult = f'{u} * {v}'
-        return (mult, str(u * v))
+    operation_and_ranges = next(OPERATION_VARIANTS)
+    operation = operation_and_ranges[0]
+    ranges = operation_and_ranges[1]
+    a = random.randint(*ranges[0])
+    b = random.randint(*ranges[1])
+    question, check = ariph_calculate(a, b, operation)
+    return (question, check)
