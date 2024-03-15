79096f99c7a5cce53c8de65cb18228c38e13033f
fomineandrei <fomineandrei@yandex.ru>
2024-03-15 12:23:16 +0300

    refactored brain_even_logic.py

--- brain_games/games/brain_even_logic.py
+++ brain_games/games/brain_even_logic.py
@@ -5,6 +5,8 @@ import random
 
 # Условия игры brain_even
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+# Диапазон проверяемых чисел
+RANGE_NUM = (1, 999)
 
 
 # Функция проверки случайных чисел на четность
@@ -15,7 +17,7 @@ def even_check(num: int) -> bool:
 
 # Функция формирования кортежа "вопрос - правильный ответ"
 def question_answer_pair():
-    num = random.randint(1, 999)
+    num = random.randint(*RANGE_NUM)
     if even_check(num) is True:
         return (num, 'yes')
     else:
